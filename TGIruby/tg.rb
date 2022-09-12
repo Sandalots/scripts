b401485ee2ace22a4f768866284e7cbb7e645a70
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-13 00:00:46 +0100

    Added ruby implementation of TGI

---
+++ TGIruby/tg.rb
@@ -0,0 +1,12 @@
+require_relative 'db.rb'
+class Main
+    def initialize(list)
+        @episodeList = list
+    end
+    def run()
+        puts(@episodeList.keys.sample)
+    end
+end
+
+s = Main.new($episodes)
+s.run
