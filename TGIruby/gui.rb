2c9ce034ce6069dfba9434761a6dff4dacbbbd98
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-14 02:46:05 +0100

    Update gui.rb

--- TGIruby/gui.rb
+++ TGIruby/gui.rb
@@ -1,9 +1,11 @@
 require_relative 'tg.rb'
 require 'tk'
 
+
 root = TkRoot.new { title "Hello, World!" }
 TkLabel.new(root) do
    text 'Hello, World!'
    pack { padx 15 ; pady 15; side 'left' }
 end
+
 Tk.mainloop
